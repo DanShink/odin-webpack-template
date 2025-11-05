0b5b899e8925914f43c3283dc7de81183668e90b
dshinkarow <daniel.shinkarow@codeware.com>
2025-11-05 13:20:58 -0500

    Complete webpack config for dev and prod

--- webpack.common.js
+++ webpack.common.js
@@ -2,17 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["src/template.html"],
-    port: 25565,
   },
   plugins: [
     new HtmlWebpackPlugin({
