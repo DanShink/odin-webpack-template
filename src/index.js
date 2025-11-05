e778d7713a93e90ed7aa659764096f8201512c86
dshinkarow <daniel.shinkarow@codeware.com>
2025-11-05 12:58:50 -0500

    Initial Template

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+import ficsitImage from "./images/ficsit.webp";
+
+console.log("Hello, World!");
+const image = document.createElement("img");
+image.src = ficsitImage;
+document.body.appendChild(image);
